14b6277ba5222bcb1640a25f94164199efc08055
sanchezKARLOS <94863844+sanchezKARLOS@users.noreply.github.com>
2022-06-22 07:51:45 -0500

    hoy22062022

--- index.php
+++ index.php
@@ -24,14 +24,11 @@
   <!-- Scrollbar Custom CSS -->
   <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
   <!-- Tweaks for older IEs-->
-  <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-<!--[if lt IE 9]>
+ <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
 </head>
 <!-- body -->
-
 <body class="main-layout">
   <!-- loader  -->
   <div class="loader_bg">
@@ -57,24 +54,21 @@
                 <div class="limit-box">
                   <nav class="main-menu ">
                     <ul class="menu-area-main">
-                      <li class="active"> <a href="index.html">Inicio</a> </li>
-                      <li> <a href="#courses">Mis cursos </a> </li>
+                      <li class="active"> <a href="index.php">Inicio</a> </li>
                       <li> <a href="#about">Saber mas</a> </li>
-                      <li> <a href="#learn">Mi perfil</a> </li>
-                      <li> <a href="#contact">Contactanos</a> </li>
+                      <li> <a href="#footer">Contactanos</a> </li>
                      </ul>
                    </nav>
                  </div>
                </div> 
                <div class="mean-last">
-                       <a href=""><a href="SESSIONES/index.php">Inicia Sesiòn</a></div> </a>           
+              <a href=""><a href="SESSIONES/index.php">Inicia Sesiòn</a></div> </a>           
              </div>
            </div>
          </div>
        </div>
      </div>
-     <!-- end header inner -->
-
+     <!-- end header inner -------->
      <!-- end header -->
      <section class="slider_section">
       <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -85,7 +79,6 @@
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item active">
-
             <div class="container-fluid padding_dd">
               <div class="carousel-caption">
                 <div class="row">
@@ -93,7 +86,6 @@
                     <div class="text-bg">
                       <h1>Software educativo</h1>
                       <p>Problemas de matematica basica</p>
-                      <a href="#">Inicio</a> <a href="#">Registrate</a>
                     </div>
                   </div>
                   <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
@@ -106,20 +98,16 @@
             </div>
           </div>
           <div class="carousel-item">
-
             <div class="container-fluid padding_dd">
               <div class="carousel-caption">
-
                 <div class="row">
                   <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
                     <div class="text-bg">
                       <h1>Encuentra los mejores cursos de matematica basica</h1>
                       <p>no pierdas la oportunidad, totalmente gratis</p>
                       <p>no tienes cuenta?</p>
-                      <a href="#">Inicio</a><a href="#">Registrar</a>
                     </div>
                   </div>
-
                   <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
                     <div class="images_box">
                       <figure></figure>
@@ -128,12 +116,8 @@
                 </div>
               </div>
             </div>
-
           </div>
-
-
           <div class="carousel-item">
-
             <div class="container-fluid padding_dd">
               <div class="carousel-caption ">
                 <div class="row">
@@ -141,7 +125,6 @@
                     <div class="text-bg">
                       <h1>Conocenos mas, te gustara ser parte de nosotros</h1>
                       <h2>conviertete en un profesinal</h2>
-                      <a href="#">Inicio</a> <a href="#">Registrate</a>
                     </div>
                   </div>
                   <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
@@ -165,13 +148,9 @@
       <span class="sr-only">Next</span>
     </a>
   </div>
-
 </section>
 </div>
 </header>
-
-
-
 <!-- about  -->
 <div id="about" class="about">
   <div class="container">
@@ -182,7 +161,6 @@
           <p> orem ipsum dolor sit amet, consectetur adipisicing elit. Quas voluptatem maiores eaque similique non distinctio voluptates perspiciatis omnis, repellendus ipsa aperiam, laudantium voluptatum nulla?.
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, assumenda, vo
           luptatum. Libero eligendi molestias iure error animi totam laudantium, aspernatur similique id eos at consectetur illo culpa,  </p>
-          <a href="Javascript:void(0)">Inicia Session</a>
         </div>
       </div>
       <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
@@ -191,227 +169,13 @@
         </div>
       </div>
     </div>
-
   </div>
 </div>
-<!-- end abouts -->
-
-
-
-<!-- our -->
-<div id="important" class="important">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="titlepage">
-          <h2>Somos <strong class="yellow">Los mejores cursos</strong></h2>
-          <span>que esperas para ser parte de nosotros, registrate, es totalmente gratis</span>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="important_bg">
-    <div class="container">
-      <div class="row">
-
-        <div class="col col-xs-12">
-          <div class="important_box">
-            <h3>200+</h3>
-            <span>Docentes</span>
-          </div>
-        </div>
-        <div class="col col-xs-12">
-          <div class="important_box">
-            <h3>50+</h3>
-            <span>Cursos</span>
-          </div>
-        </div>
-        <div class="col col-xs-12">
-          <div class="important_box">
-            <h3>200+</h3>
-            <span>Miembros</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-
-<!-- end our -->
-<!-- Courses -->
-<div id="courses" class="Courses">
-  <div class="container-fluid padding_left3">
-    <div class="row">
-      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-        <div class="box_bg">
-          <div class="box_bg_img">
-            <div class="row">
-              <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                <div class="box_my">
-                  <figure><img src="images/my1.jpg"></figure>
-                  <div class="overlay">
-                    <h3>Estructura de datos</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content o</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                <div class="box_my">
-                  <figure><img src="images/my2.jpg"></figure>
-                  <div class="overlay">
-                    <h3>Cinematografia</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content o</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                <div class="box_my">
-                  <figure><img src="images/my3.jpg"></figure>
-                  <div class="overlay">
-                    <h3>Habilidades</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content o</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                <div class="box_my">
-                  <figure><img src="images/my4.jpg"></figure>
-                  <div class="overlay">
-                    <h3>ciencias tecnica</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content o</p>
-                  </div>
-                </div>
-              </div>
-
-
-
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 border_right">
-        <div class="box_text">
-          <div class="titlepage">
-            <h2>Mi <strong class="yellow"> Cursos</strong></h2>
-          </div>
-          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-          <a href="Javascript:void(0)">Inica Sesion</a>
-        </div>
-      </div> 
-    </div>
-  </div>
-</div>
-<!-- end Courses -->
-
-<!-- learn -->
-
-
-<div id="learn" class="learn">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="titlepage">
-          <h2>escribenos <strong class="yellow">te gustara   estar con nosotros</strong></h2>
-          <span>packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</span>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <div class="learn_box">
-          <figure><img src="images/img.jpg" alt="img"/></figure>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- MAKE --> 
-<div class="make">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="titlepage">
-          <h2>HAS QUE <strong class="white_colo">TUS CURSOS DESTAQUEN</strong></h2>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
-        <div class="make_text">
-          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-          </p>
-          <a href="Javascript:void(0)">Registrate</a>
-        </div>
-      </div>
-      <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
-        <div class="make_img">
-          <figure><img src="images/make_img.jpg"></figure>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- end MAKE --> 
-<!-- end learn --> 
-
-
-<!-- contact -->
-<div id="contact" class="contact">
-  <div class="container-fluid padding_left2">
-    <div class="white_color">
-      <div class="row">
-
-        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-          <div id="map">
-          </div>
-
-        </div>
-        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-
-          <form class="contact_bg">
-            <div class="row">
-              <div class="col-md-12">
-                <div class="titlepage">
-                  <h2>Contactanos <strong class="yellow"></strong></h2>
-                </div>
-                <div class="col-md-12">
-                  <input class="contactus" placeholder="Nombre" type="text" name="Your Name">
-                </div>
-                <div class="col-md-12">
-                  <input class="contactus" placeholder="Correo Electronico" type="text" name="Your Email">
-                </div>
-                <div class="col-md-12">
-                  <input class="contactus" placeholder="Telefono-Celular" type="text" name="Your Phone">
-                </div>
-                <div class="col-md-12">
-                  <textarea class="textarea" placeholder="Messages" type="text" name="Message"></textarea>
-                </div>
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                  <button class="send">Enviar</button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-    <!-- end contact -->
-
     <!--  footer -->
-    <footr>
+    <footer id="footer">
       <div class="footer ">
         <div class="container">
           <div class="row">
-
-            <div class="col-md-12">
-              <form class="news">
-                <input class="newslatter" placeholder="Correo Electronico" type="text" name=" Email">
-                <button class="submit">Subscribete</button>
-              </form>
-            </div>
             <div class="col-md-12">
               <h2>Nuestro Contactos</h2>
               <span>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in  </span>
@@ -492,36 +256,6 @@
           <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
           <script src="js/custom.js"></script>
           <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
-
-
-          <script>
-// This example adds a marker to indicate the position of Bondi Beach in Sydney,
-// Australia.
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 11,
-    center: {
-      lat: 40.645037,
-      lng: -73.880224
-    },
-  });
-
-  var image = 'images/maps-and-flags.png';
-  var beachMarker = new google.maps.Marker({
-    position: {
-      lat: 40.645037,
-      lng: -73.880224
-    },
-    map: map,
-    icon: image
-  });
-}
-</script>
-<!-- google map js -->
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap"></script>
-<!-- end google map js -->
-
-
 
 </body>
 
